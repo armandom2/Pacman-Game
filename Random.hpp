be488d8b22c7e92b1488ff60bf85eec8e874b909
armandomiguel <armando.miguel518@gmail.com>
2018-10-03 12:49:08 -0700

    The class of random which hold all the functions for Random.cpp.

---
+++ Random.hpp
@@ -0,0 +1,9 @@
+#ifndef Random_hpp
+#define Random_hpp
+
+#include <stdio.h>
+#include <SFML/Graphics.hpp>
+#include <random>
+#include <cmath>
+    int generateRandom(int max);
+#endif /* Random_hpp */
